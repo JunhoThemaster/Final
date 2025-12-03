637232424767d4b4c01024e878a251df5f9370b6
서준호 <seojunho22@gmail.com>
2025-12-03 10:20:52 +0900

    Refactor JWT handler for clarity and secret key
    Updated the SECRET_KEY to be empty and modified comments for clarity.

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -4,12 +4,11 @@ import jwt
 from datetime import datetime, timedelta
 from fastapi import HTTPException
 
-# 👉 환경 변수에서 불러오도록 수정 가능
-SECRET_KEY = "66a1d5781cc0ed773fd7899986441b110a85ce4d8ca1d19dd1a87e01a2a343bc"
+SECRET_KEY = ""
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 1200
 
-# ✅ JWT 생성 함수
+#  JWT 생성 함수
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
@@ -17,11 +16,11 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-# ✅ JWT 디코딩 함수 (검증 포함)
+#  JWT 디코딩 함수 (검증 포함)
 def decode_jwt_token(token: str) -> dict | None:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload  # 예: {"sub": "user_id"}
+        return payload  #  {"sub": "user_id"}
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="토큰이 만료되었습니다.")
     except jwt.InvalidTokenError:
